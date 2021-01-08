4202d985d50121a46f2d28b505ab6604c889828f
leondgarse <leondgarse@gmail.com>
2021-01-08 15:48:38 +0800

    Add keras ghost_net.py

---
+++ backbones/ghost_model.py
@@ -0,0 +1,137 @@
+"""
+The main GhostNet architecture as specified in "GhostNet: More Features from Cheap Operations"
+Paper:
+https://arxiv.org/pdf/1911.11907.pdf
+"""
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import (
+    Activation,
+    Add,
+    BatchNormalization,
+    Concatenate,
+    Conv2D,
+    DepthwiseConv2D,
+    GlobalAveragePooling2D,
+    Input,
+    Reshape,
+    Multiply,
+)
+import math
+
+def _make_divisible(v, divisor=4, min_value=None):
+    """
+    This function is taken from the original tf repo.
+    It ensures that all layers have a channel number that is divisible by 8
+    It can be seen here:
+    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    """
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
+
+
+def se_module(inputs, se_ratio=0.25):
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+    filters = inputs.shape[channel_axis]
+    reduction = _make_divisible(filters * se_ratio)
+    se = GlobalAveragePooling2D()(inputs)
+    se = Reshape((1, 1, filters))(se)
+    se = Conv2D(reduction, kernel_size=1, use_bias=True)(se)
+    # se = PReLU(shared_axes=[1, 2])(se)
+    se = Activation('relu')(se)
+    se = Conv2D(filters, kernel_size=1, use_bias=True)(se)
+    se = Activation('hard_sigmoid')(se)
+    return Multiply()([inputs, se])
+
+
+def ghost_module(inputs, out, convkernel=1, dwkernel=3, add_activation=True):
+    # conv_out_channel = math.ceil(out * 1.0 / 2)
+    conv_out_channel = out // 2
+    # tf.print("[ghost_module] out:", out, "conv_out_channel:", conv_out_channel)
+    cc = Conv2D(conv_out_channel, convkernel, use_bias=False, strides=(1, 1), padding='same')(inputs)   # padding=kernel_size//2
+    cc = BatchNormalization(axis=-1)(cc)
+    if add_activation:
+        cc = Activation('relu')(cc)
+
+    channel = int(out - conv_out_channel)
+    nn = DepthwiseConv2D(dwkernel, 1, padding='same', use_bias=False)(cc)   # padding=dw_size//2
+    nn = BatchNormalization(axis=-1)(nn)
+    if add_activation:
+        nn = Activation('relu')(nn)
+    # tf.print("[ghost_module] nn.shape:", nn.shape, "channel:", channel)
+    # nn = nn[:, :, :, :channel]
+    # nn = tf.gather(nn, range(channel), axis=-1)
+    return Concatenate()([cc, nn])
+
+
+def ghost_bottleneck(inputs, dwkernel, strides, exp, out, se_ratio=0, shortcut=True):
+    nn = ghost_module(inputs, exp, add_activation=True) # ghost1 = GhostModule(in_chs, exp, relu=True)
+    # nn = BatchNormalization(axis=-1)(nn)
+    # nn = Activation('relu')(nn)
+    if strides > 1:
+        # Extra depth conv if strides higher than 1
+        nn = DepthwiseConv2D(dwkernel, strides, padding='same', use_bias=False)(nn)
+        nn = BatchNormalization(axis=-1)(nn)
+        # nn = Activation('relu')(nn)
+
+    if se_ratio > 0:
+        # Squeeze and excite
+        nn = se_module(nn, se_ratio)   # se = SqueezeExcite(exp, se_ratio=se_ratio)
+
+    # Point-wise linear projection
+    nn = ghost_module(nn, out, add_activation=False) # ghost2 = GhostModule(exp, out, relu=False)
+    # nn = BatchNormalization(axis=-1)(nn)
+
+    if shortcut:
+        xx = DepthwiseConv2D(dwkernel, strides, padding='same', use_bias=False)(inputs) # padding=(dw_kernel_size-1)//2
+        xx = BatchNormalization(axis=-1)(xx)
+        xx = Conv2D(out, (1, 1), strides=(1, 1), padding='valid', use_bias=False)(xx)    # padding=0
+        xx = BatchNormalization(axis=-1)(xx)
+    else:
+        xx = inputs
+    return Add()([xx, nn])
+
+
+def GhostNet(input_shape=(224, 224, 3), include_top=True, classes=0, width=1.3, name="GhostNet"):
+    inputs = Input(shape=input_shape)
+    out_channel = _make_divisible(16 * width, 4)
+    nn = Conv2D(out_channel, (3, 3), strides=(2, 2), padding='same', use_bias=False)(inputs)  # padding=1
+    nn = BatchNormalization(axis=-1)(nn)
+    nn = Activation('relu')(nn)
+    # nn = Conv2D(960, (1, 1), strides=(1, 1), padding='same', use_bias=False)(nn)
+
+    dwkernels = [3, 3, 3, 5, 5, 3, 3, 3, 3, 3, 3, 5, 5, 5, 5, 5]
+    exps = [16, 48, 72, 72, 120, 240, 200, 184, 184, 480, 672, 672, 960, 960, 960, 512]
+    outs = [16, 24, 24, 40, 40, 80, 80, 80, 80, 112, 112, 160, 160, 160, 160, 160]
+    use_ses = [0, 0, 0, 0.25, 0.25, 0, 0, 0, 0, 0.25, 0.25, 0.25, 0, 0.25, 0, 0.25]
+    strides = [1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1]
+
+    pre_out = out_channel
+    for dwk, stride, exp, out, se in zip(dwkernels, strides, exps, outs, use_ses):
+        out = _make_divisible(out * width, 4)
+        exp = _make_divisible(exp * width, 4)
+        shortcut = False if out == pre_out and stride == 1 else True
+        nn = ghost_bottleneck(nn, dwk, stride, exp, out, se, shortcut)
+        pre_out = out
+
+    out = _make_divisible(exps[-1] * width, 4)
+    nn = Conv2D(out, (1, 1), strides=(1, 1), padding='valid', use_bias=False)(nn)  # padding=0
+    nn = BatchNormalization(axis=-1)(nn)
+    nn = Activation('relu')(nn)
+
+    if include_top:
+        nn = GlobalAveragePooling2D()(nn)
+        nn = Reshape((1, 1, int(nn.shape[1])))(nn)
+        nn = Conv2D(1280, (1, 1), strides=(1, 1), padding='same', use_bias=False)(nn)
+        nn = BatchNormalization(axis=-1)(nn)
+        nn = Activation('relu')(nn)
+
+        nn = Conv2D(classes, (1, 1), strides=(1, 1), padding='same', use_bias=False)(nn)
+        nn = K.squeeze(nn, 1)
+        nn = Activation('softmax')(nn)
+    return Model(inputs=inputs, outputs=nn, name=name)
