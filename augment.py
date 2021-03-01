364469753dcb700765873923fa8e1fdffeb7df98
leondgarse <leondgarse@gmail.com>
2021-03-01 20:58:43 +0800

    Update for readme

--- augment.py
+++ augment.py
@@ -1,3 +1,6 @@
+"""
+Copied from https://github.com/tensorflow/models/blob/master/official/vision/image_classification/augment.py
+"""
 # Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
