adff9ae03b809df38d2d16c7bf5593233d8ab94f
leondgarse <leondgarse@gmail.com>
2021-03-07 14:53:45 +0800

    Remove future in augment.py

--- augment.py
+++ augment.py
@@ -21,11 +21,6 @@ AutoAugment Reference: https://arxiv.org/abs/1805.09501
 RandAugment Reference: https://arxiv.org/abs/1909.13719
 """
 
-from __future__ import absolute_import
-from __future__ import division
-# from __future__ import google_type_annotations
-from __future__ import print_function
-
 import math
 
 import tensorflow as tf
