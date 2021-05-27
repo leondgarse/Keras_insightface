6f58f6c18145aa9906c7241c9deb00856641daac
leondgarse <leondgarse@gmail.com>
2021-05-27 15:55:48 +0800

    Update BotNet50V2 / BotNet101V2

--- backbones/botnet.py
+++ backbones/botnet.py
@@ -23,23 +23,24 @@ class MHSAWithRelativePosition(keras.layers.MultiHeadAttention):
         super(MHSAWithRelativePosition, self).__init__(num_heads=num_heads, key_dim=self.key_dim, **kwargs)
         self.num_heads, self.bottleneck_dimension, self.relative = num_heads, bottleneck_dimension, relative
 
-    def _build_from_signature(self, featuremap):
-        super(MHSAWithRelativePosition, self)._build_from_signature(query=featuremap, value=featuremap)
-        _, hh, ww, _ = featuremap.shape
+    def _build_from_signature(self, query, value, key=None):
+        super(MHSAWithRelativePosition, self)._build_from_signature(query=query, value=value)
+        if hasattr(query, "shape"):
+            _, hh, ww, _ = query.shape
+        else:
+            _, hh, ww, _ = query
         stddev = self.key_dim ** -0.5
         self.rel_emb_w = self.add_weight(
             name="r_width",
             shape=(self.key_dim, 2 * ww - 1),
             initializer=tf.random_normal_initializer(stddev=stddev),
             trainable=True,
-            dtype=featuremap.dtype,
         )
         self.rel_emb_h = self.add_weight(
             name="r_height",
             shape=(self.key_dim, 2 * hh - 1),
             initializer=tf.random_normal_initializer(stddev=stddev),
             trainable=True,
-            dtype=featuremap.dtype,
         )
 
     def get_config(self):
@@ -89,7 +90,7 @@ class MHSAWithRelativePosition(keras.layers.MultiHeadAttention):
 
     def call(self, inputs, attention_mask=None, return_attention_scores=False, training=None):
         if not self._built_from_signature:
-            self._build_from_signature(featuremap=inputs)
+            self._build_from_signature(query=inputs, value=inputs)
         #   N = `num_attention_heads`
         #   H = `size_per_head`
         # `query` = [B, T, N ,H]
@@ -205,6 +206,72 @@ def bot_stack(
     return featuremap
 
 
+def bot_block_2(
+    featuremap,
+    heads=4,
+    proj_factor=4,
+    activation="relu",
+    pos_enc_type="relative",
+    strides=1,
+    target_dimension=2048,
+    conv_shortcut=False,
+    use_MHSA=True,
+    name="all2all",
+):
+    preact = batchnorm_with_activation(featuremap, activation=activation, name=name + "_preact_")
+
+    if conv_shortcut:
+        shortcut = conv2d_no_bias(preact, target_dimension, 1, strides=strides, name=name + "_0_")
+    else:
+        shortcut = layers.MaxPooling2D(1, strides=strides)(featuremap) if strides > 1 else featuremap
+
+    bottleneck_dimension = target_dimension // proj_factor
+    nn = conv2d_no_bias(preact, bottleneck_dimension, 1, strides=1, name=name + "_1_")
+    nn = batchnorm_with_activation(nn, activation=activation, name=name + "_1_")
+
+    if use_MHSA:    # BotNet block
+        nn = MHSAWithRelativePosition(num_heads=heads, bottleneck_dimension=bottleneck_dimension, name=name + "_2_mhsa")(nn)
+        if strides != 1:
+            nn = layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding="same", name=name + '_2_pool')(nn)
+    else:   # ResNet block
+        nn = layers.ZeroPadding2D(padding=((1, 1), (1, 1)), name=name + '_2_pad')(nn)
+        nn = conv2d_no_bias(nn, bottleneck_dimension, 3, strides=strides, name=name + "_2_")
+    nn = batchnorm_with_activation(nn, activation=activation, name=name + "_2_")
+
+    nn = conv2d_no_bias(nn, target_dimension, 1, strides=1, name=name + "_3_")
+    nn = layers.Add(name=name + '_out')([shortcut, nn])
+    return nn
+
+
+def bot_stack_2(
+    featuremap,
+    target_dimension=2048,
+    num_layers=3,
+    strides=2,
+    activation="relu",
+    heads=4,
+    proj_factor=4,
+    pos_enc_type="relative",
+    name="all2all_stack",
+    use_MHSA=True,
+):
+    """ c5 Blockgroup of BoT Blocks. Use `activation=swish` for `silu` """
+    for ii in range(num_layers):
+        featuremap = bot_block_2(
+            featuremap,
+            heads=heads,
+            proj_factor=proj_factor,
+            activation=activation,
+            pos_enc_type=pos_enc_type,
+            strides=strides if ii == num_layers - 1 else 1,
+            target_dimension=target_dimension,
+            conv_shortcut=True if ii == 0 else False,
+            use_MHSA=use_MHSA,
+            name=name + "_block{}".format(ii+1),
+        )
+    return featuremap
+
+
 def BotNet(
     stack_fn,
     preact,
@@ -215,12 +282,16 @@ def BotNet(
     weights=None,
     input_shape=None,
     classes=1000,
+    use_conv3_stride1=False,
     classifier_activation="softmax",
 ):
     img_input = layers.Input(shape=input_shape)
 
-    nn = layers.ZeroPadding2D(padding=((3, 3), (3, 3)), name="conv1_pad")(img_input)
-    nn = layers.Conv2D(64, 7, strides=2, use_bias=use_bias, name="conv1_conv")(nn)
+    if use_conv3_stride1:
+        nn = layers.Conv2D(64, 3, strides=1, use_bias=use_bias, padding="SAME", name="conv1_conv")(img_input)
+    else:
+        nn = layers.ZeroPadding2D(padding=((3, 3), (3, 3)), name="conv1_pad")(img_input)
+        nn = layers.Conv2D(64, 7, strides=2, use_bias=use_bias, name="conv1_conv")(nn)
 
     if not preact:
         nn = batchnorm_with_activation(nn, activation=activation, zero_gamma=False, name="conv1_")
@@ -236,34 +307,53 @@ def BotNet(
     return keras.models.Model(img_input, nn, name=model_name)
 
 
-def BotNet50(strides=2, activation="relu", include_top=True, weights=None, input_shape=None, classes=1000, **kwargs):
+def BotNet50(strides=2, activation="relu", include_top=True, weights=None, input_shape=None, classes=1000, use_MHSA=True, **kwargs):
     def stack_fn(nn):
         nn = bot_stack(nn, 64 * 4, 3, strides=1, activation=activation, name="conv2", use_MHSA=False)
         nn = bot_stack(nn, 128 * 4, 4, strides=2, activation=activation, name="conv3", use_MHSA=False)
         nn = bot_stack(nn, 256 * 4, 6, strides=2, activation=activation, name="conv4", use_MHSA=False)
-        nn = bot_stack(nn, 512 * 4, 3, strides=strides, activation=activation, use_MHSA=True)
+        nn = bot_stack(nn, 512 * 4, 3, strides=strides, activation=activation, use_MHSA=use_MHSA)
         return nn
 
     return BotNet(stack_fn, False, True, "botnet50", activation, include_top, weights, input_shape, classes, **kwargs)
 
 
-def BotNet101(strides=2, activation="relu", include_top=True, weights=None, input_shape=None, classes=1000, **kwargs):
+def BotNet101(strides=2, activation="relu", include_top=True, weights=None, input_shape=None, classes=1000, use_MHSA=True, **kwargs):
     def stack_fn(nn):
         nn = bot_stack(nn, 64 * 4, 3, strides=1, activation=activation, name="conv2", use_MHSA=False)
         nn = bot_stack(nn, 128 * 4, 4, strides=2, activation=activation, name="conv3", use_MHSA=False)
         nn = bot_stack(nn, 256 * 4, 23, strides=2, activation=activation, name="conv4", use_MHSA=False)
-        nn = bot_stack(nn, 512 * 4, 3, strides=strides, activation=activation, use_MHSA=True)
+        nn = bot_stack(nn, 512 * 4, 3, strides=strides, activation=activation, use_MHSA=use_MHSA)
         return nn
 
     return BotNet(stack_fn, False, True, "botnet50", activation, include_top, weights, input_shape, classes, **kwargs)
 
 
-def BotNet152(strides=2, activation="relu", include_top=True, weights=None, input_shape=None, classes=1000, **kwargs):
+def BotNet152(strides=2, activation="relu", include_top=True, weights=None, input_shape=None, classes=1000, use_MHSA=True, **kwargs):
     def stack_fn(nn):
         nn = bot_stack(nn, 64 * 4, 3, strides=1, activation=activation, name="conv2", use_MHSA=False)
         nn = bot_stack(nn, 128 * 4, 8, strides=2, activation=activation, name="conv3", use_MHSA=False)
         nn = bot_stack(nn, 256 * 4, 36, strides=2, activation=activation, name="conv4", use_MHSA=False)
-        nn = bot_stack(nn, 512 * 4, 3, strides=strides, activation=activation, use_MHSA=True)
+        nn = bot_stack(nn, 512 * 4, 3, strides=strides, activation=activation, use_MHSA=use_MHSA)
         return nn
 
     return BotNet(stack_fn, False, True, "botnet50", activation, include_top, weights, input_shape, classes, **kwargs)
+
+
+def BotNet50V2(strides=2, activation="relu", include_top=True, weights=None, input_shape=None, classes=1000, use_MHSA=True, **kwargs):
+    def stack_fn(nn):
+        nn = bot_stack_2(nn, 64 * 4, 3, name='conv2', use_MHSA=False)
+        nn = bot_stack_2(nn, 128 * 4, 4, name='conv3', use_MHSA=False)
+        nn = bot_stack_2(nn, 256 * 4, 6, strides=strides, name='conv4', use_MHSA=False)
+        return bot_stack_2(nn, 512 * 4, 3, strides=1, name='conv5', use_MHSA=use_MHSA)
+
+    return BotNet(stack_fn, True, True, "botnet50v2", activation, include_top, weights, input_shape, classes, **kwargs)
+
+def BotNet101V2(strides=2, activation="relu", include_top=True, weights=None, input_shape=None, classes=1000, use_MHSA=True, **kwargs):
+    def stack_fn(nn):
+        nn = bot_stack_2(nn, 64 * 4, 3, name='conv2', use_MHSA=False)
+        nn = bot_stack_2(nn, 128 * 4, 4, name='conv3', use_MHSA=False)
+        nn = bot_stack_2(nn, 256 * 4, 23, strides=strides, name='conv4', use_MHSA=False)
+        return bot_stack_2(nn, 512 * 4, 3, strides=1, name='conv5', use_MHSA=use_MHSA)
+
+    return BotNet(stack_fn, True, True, "botnet101v2", activation, include_top, weights, input_shape, classes, **kwargs)
