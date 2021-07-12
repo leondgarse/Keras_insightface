71d827d78a3e98fd4e177353af70d74a6ebb6fd7
leondgarse <leondgarse@gmail.com>
2021-07-12 16:10:31 +0800

    reformat backbones/resnest.py

--- backbones/resnest.py
+++ backbones/resnest.py
@@ -1,206 +1,141 @@
-# Copied from https://github.com/QiaoranC/tf_ResNeSt_RegNet_model, and modified
-
 import tensorflow as tf
-from tensorflow.keras.activations import softmax
-from tensorflow.keras import models
-from tensorflow.keras.layers import (
-    AveragePooling2D,
-    Activation,
-    Input,
-    Conv2D,
-    BatchNormalization,
-    GlobalAveragePooling2D,
-    MaxPool2D,
-    Add,
-)
+from tensorflow.python.keras import layers
+from tensorflow.keras import backend as K
+
+BATCH_NORM_DECAY = 0.9
+BATCH_NORM_EPSILON = 1e-5
+CONV_KERNEL_INITIALIZER = tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_out", distribution="truncated_normal")
+# CONV_KERNEL_INITIALIZER = 'glorot_uniform'
 
 
-class GroupedConv2D(object):
-    """Groupped convolution.
-    https://github.com/tensorflow/tpu/blob/master/models/official/mnasnet/mixnet/custom_layers.py
-    Currently tf.keras and tf.layers don't support group convolution, so here we
-    use split/concat to implement this op. It reuses kernel_size for group
-    definition, where len(kernel_size) is number of groups. Notably, it allows
-    different group has different kernel size.
-    """
-
-    def __init__(self, filters, kernel_size, **kwargs):
-        """Initialize the layer.
-        Args:
-        filters: Integer, the dimensionality of the output space.
-        kernel_size: An integer or a list. If it is a single integer, then it is
-            same as the original Conv2D. If it is a list, then we split the channels
-            and perform different kernel for each group.
-        **kwargs: other parameters passed to the original conv2d layer.
-        """
-        self._groups = len(kernel_size)
-        self._channel_axis = -1
-
-        self._convs = []
-        splits = self._split_channels(filters, self._groups)
-        for i in range(self._groups):
-            self._convs.append(self._get_conv2d(splits[i], kernel_size[i], **kwargs))
-
-    def _get_conv2d(self, filters, kernel_size, **kwargs):
-        """A helper function to create Conv2D layer."""
-        return Conv2D(filters=filters, kernel_size=kernel_size, **kwargs)
-
-    def _split_channels(self, total_filters, num_groups):
-        split = [total_filters // num_groups for _ in range(num_groups)]
-        split[0] += total_filters - sum(split)
-        return split
-
-    def __call__(self, inputs):
-        if len(self._convs) == 1:
-            return self._convs[0](inputs)
-
-        if tf.__version__ < "2.0.0":
-            filters = inputs.shape[self._channel_axis].value
-        else:
-            filters = inputs.shape[self._channel_axis]
-        splits = self._split_channels(filters, len(self._convs))
-        x_splits = tf.split(inputs, splits, self._channel_axis)
-        x_outputs = [c(x) for x, c in zip(x_splits, self._convs)]
-        x = tf.concat(x_outputs, self._channel_axis)
-        return x
+def batchnorm_with_activation(inputs, activation="relu", name=""):
+    """Performs a batch normalization followed by an activation. """
+    bn_axis = 3 if K.image_data_format() == "channels_last" else 1
+    nn = layers.BatchNormalization(
+        axis=bn_axis,
+        momentum=BATCH_NORM_DECAY,
+        epsilon=BATCH_NORM_EPSILON,
+        name=name + "bn",
+    )(inputs)
+    if activation:
+        nn = layers.Activation(activation=activation, name=name + activation)(nn)
+    return nn
 
 
-class ResNest:
-    def __init__(
-        self,
-        input_shape=(224, 224, 3),
-        active="relu",
-        blocks_set=[3, 4, 6, 3],
-        radix=2,
-        groups=1,
-        stem_width=32,
-        name="model",
-    ):
-        self.channel_axis = -1  # not for change
-        self.active = active  # default relu
-        self.input_shape = input_shape
-        self.name = name
-        self.blocks_set = blocks_set
-        self.radix = radix
-        self.stem_width = stem_width
-
-    def _make_stem(self, input_tensor, stem_width=64):
-        x = Conv2D(stem_width, kernel_size=3, strides=2, padding="same", kernel_initializer="he_normal", use_bias=False)(
-            input_tensor
-        )
-        x = BatchNormalization(axis=self.channel_axis, epsilon=1.001e-5)(x)
-        x = Activation(self.active)(x)
-        x = Conv2D(stem_width, kernel_size=3, strides=1, padding="same", kernel_initializer="he_normal", use_bias=False)(x)
-        x = BatchNormalization(axis=self.channel_axis, epsilon=1.001e-5)(x)
-        x = Activation(self.active)(x)
-        x = Conv2D(stem_width * 2, kernel_size=3, strides=1, padding="same", kernel_initializer="he_normal", use_bias=False)(x)
-
-        return x
-
-    def _rsoftmax(self, input_tensor, filters, radix, groups):
-        x = input_tensor
-        batch = x.shape[0]
-        x = tf.reshape(x, [-1, groups, radix, filters // groups])
-        x = tf.transpose(x, [0, 2, 1, 3])
-        x = softmax(x, axis=1)
-        x = tf.reshape(x, [-1, 1, 1, radix * filters])
-        return x
-
-    def _SplAtConv2d(self, input_tensor, filters=64, kernel_size=3, stride=1, groups=1, radix=0):
-        x = input_tensor
-        in_channels = input_tensor.shape[-1]
-
-        x = GroupedConv2D(
-            filters=filters * radix,
-            kernel_size=[kernel_size for i in range(groups * radix)],
-            padding="same",
-            kernel_initializer="he_normal",
-            use_bias=False,
-        )(x)
-
-        x = BatchNormalization(axis=self.channel_axis, epsilon=1.001e-5)(x)
-        x = Activation(self.active)(x)
-
-        batch, rchannel = x.shape[0], x.shape[-1]
-        splited = tf.split(x, radix, axis=-1)
-        gap = sum(splited)
-
-        # print('sum',gap.shape)
-        gap = GlobalAveragePooling2D()(gap)
-        gap = tf.reshape(gap, [-1, 1, 1, filters])
-        # print('adaptive_avg_pool2d',gap.shape)
-
-        reduction_factor = 4
-        inter_channels = max(in_channels * radix // reduction_factor, 32)
-
-        x = Conv2D(inter_channels, kernel_size=1)(gap)
-
-        x = BatchNormalization(axis=self.channel_axis, epsilon=1.001e-5)(x)
-        x = Activation(self.active)(x)
-        x = Conv2D(filters * radix, kernel_size=1)(x)
-
-        atten = self._rsoftmax(x, filters, radix, groups)
-
-        logits = tf.split(atten, radix, axis=-1)
-        out = sum([a * b for a, b in zip(splited, logits)])
-        return out
-
-    def _make_block(self, input_tensor, filters=64, stride=2, radix=1):
-        x = input_tensor
-        inplanes = input_tensor.shape[-1]
-        if stride != 1 or inplanes != filters * 4:
-            short_cut = input_tensor
-            short_cut = AveragePooling2D(pool_size=stride, strides=stride, padding="same")(short_cut)
-            short_cut = Conv2D(
-                filters * 4, kernel_size=1, strides=1, padding="same", kernel_initializer="he_normal", use_bias=False
-            )(short_cut)
-            short_cut = BatchNormalization(axis=self.channel_axis, epsilon=1.001e-5)(short_cut)
-        else:
-            short_cut = input_tensor
-
-        group_width = filters
-        x = Conv2D(group_width, kernel_size=1, strides=1, padding="same", kernel_initializer="he_normal", use_bias=False)(x)
-        x = BatchNormalization(axis=self.channel_axis, epsilon=1.001e-5)(x)
-        x = Activation(self.active)(x)
-
-        x = self._SplAtConv2d(x, filters=group_width, kernel_size=3, stride=1, groups=1, radix=radix)
-        if stride > 1:
-            x = AveragePooling2D(pool_size=3, strides=stride, padding="same")(x)
-            # print('can')
-        x = Conv2D(filters * 4, kernel_size=1, strides=1, padding="same", kernel_initializer="he_normal", use_bias=False)(x)
-        x = BatchNormalization(axis=self.channel_axis, epsilon=1.001e-5)(x)
-
-        m2 = Add()([x, short_cut])
-        m2 = Activation(self.active)(m2)
-        return m2
-
-    def _make_layer(self, input_tensor, blocks=4, filters=64, stride=2):
-        x = self._make_block(input_tensor, filters=filters, stride=stride, radix=2)
-        for i in range(1, blocks):
-            x = self._make_block(x, filters=filters, stride=1, radix=2)
-        return x
-
-    def build(self):
-        input_sig = Input(shape=self.input_shape)
-        x = self._make_stem(input_sig, stem_width=self.stem_width)
-
-        x = BatchNormalization(axis=self.channel_axis, epsilon=1.001e-5)(x)
-        x = Activation(self.active)(x)
-        x = MaxPool2D(pool_size=3, strides=2, padding="same")(x)
-
-        x = self._make_layer(x, blocks=self.blocks_set[0], filters=64, stride=1)
-        x = self._make_layer(x, blocks=self.blocks_set[1], filters=128, stride=2)
-        x = self._make_layer(x, blocks=self.blocks_set[2], filters=256, stride=2)
-        x = self._make_layer(x, blocks=self.blocks_set[3], filters=512, stride=2)
-
-        model = models.Model(inputs=input_sig, outputs=x, name=self.name)
-
-        return model
+def conv2d_no_bias(inputs, filters, kernel_size, strides=1, padding="VALID", name="", **kwargs):
+    return layers.Conv2D(
+        filters,
+        kernel_size,
+        strides=strides,
+        padding=padding,
+        use_bias=False,
+        kernel_initializer=CONV_KERNEL_INITIALIZER,
+        name=name + "conv",
+        **kwargs,
+    )(inputs)
 
 
-def ResNest50(input_shape=(112, 112, 3), model_name="ResNest50", **kwargs):
-    return ResNest(input_shape=input_shape, blocks_set=[3, 4, 6, 3], stem_width=32, name=model_name, **kwargs).build()
+def rsoftmax(inputs, filters, radix, groups):
+    if radix > 1:
+        nn = tf.reshape(inputs, [-1, groups, radix, filters // groups])
+        nn = K.softmax(nn, axis=2)
+        nn = tf.reshape(nn, [-1, 1, 1, radix * filters])
+    else:
+        nn = layers.Activation("sigmoid")(inputs)
+    return nn
 
 
-def ResNest101(input_shape=(112, 112, 3), model_name="ResNest101", **kwargs):
-    return ResNest(input_shape=input_shape, blocks_set=[3, 4, 23, 3], stem_width=64, name=model_name, **kwargs).build()
+def split_attention_conv2d(inputs, filters, kernel_size=3, groups=1, radix=2, activation="relu", name=""):
+    h_axis, w_axis = [2, 3] if K.image_data_format() == "channels_first" else [1, 2]
+    in_channels = inputs.shape[-1]
+    logits = conv2d_no_bias(inputs, filters * radix, kernel_size, padding="same", groups=groups * radix, name=name + "1_")
+    logits = batchnorm_with_activation(logits, activation=activation, name=name + "1_")
+
+    if radix > 1:
+        splited = tf.split(logits, radix, axis=-1)
+        gap = tf.reduce_sum(splited, axis=0)
+    else:
+        gap = logits
+    gap = tf.reduce_mean(gap, [h_axis, w_axis], keepdims=True)
+
+    reduction_factor = 4
+    inter_channels = max(in_channels * radix // reduction_factor, 32)
+    atten = layers.Conv2D(inter_channels, kernel_size=1, name=name + "2_")(gap)
+    atten = batchnorm_with_activation(atten, activation=activation, name=name + "2_")
+    atten = layers.Conv2D(filters * radix, kernel_size=1, name=name + "3_")(atten)
+    atten = rsoftmax(atten, filters, radix, groups)
+    out = layers.Multiply()([atten, logits])
+
+    if radix > 1:
+        out = tf.split(out, radix, axis=-1)
+        out = tf.reduce_sum(out, axis=0)
+    return out
+
+
+def block(inputs, filters, strides=1, activation="relu", use_se=False, radix=2, name=""):
+    if strides != 1 or inputs.shape[-1] != filters * 4:
+        short_cut = layers.AveragePooling2D(pool_size=strides, strides=strides, padding="same", name=name + "st_pool")(inputs)
+        short_cut = conv2d_no_bias(short_cut, filters * 4, kernel_size=1, padding="same", name=name + "shortcut_")
+        short_cut = batchnorm_with_activation(short_cut, activation=None, name=name + "_shortcut_")
+    else:
+        short_cut = inputs
+
+    nn = conv2d_no_bias(inputs, filters, 1, padding="same", name=name + "1_")
+    nn = batchnorm_with_activation(nn, activation=activation, name=name + "1_")
+
+    nn = split_attention_conv2d(nn, filters=filters, kernel_size=3, groups=1, radix=radix, name=name + "sa_")
+    if strides > 1:
+        nn = layers.AveragePooling2D(pool_size=3, strides=strides, padding="same", name=name + "pool_")(nn)
+    nn = conv2d_no_bias(nn, filters * 4, 1, padding="same", name=name + "2_")
+    nn = batchnorm_with_activation(nn, activation=None, name=name + "2_")
+
+    nn = layers.Add()([short_cut, nn])
+    nn = layers.Activation(activation)(nn)
+    return nn
+
+
+def stack(inputs, blocks, filters, strides, radix=2, name=""):
+    nn = block(inputs, filters=filters, strides=strides, radix=radix, name=name+"block1_")
+    for ii in range(2, blocks + 1):
+        nn = block(nn, filters=filters, strides=1, radix=radix, name=name+"block{}_".format(ii))
+    return nn
+
+
+def stem(inputs, stem_width, activation="relu", deep_stem=False, name=""):
+    if deep_stem:
+        nn = conv2d_no_bias(inputs, stem_width, 3, strides=2, padding="same", name=name+"1_")
+        nn = batchnorm_with_activation(nn, activation=activation, name=name+"1_")
+        nn = conv2d_no_bias(nn, stem_width, 3, strides=1, padding="same", name=name+"2_")
+        nn = batchnorm_with_activation(nn, activation=activation, name=name+"2_")
+        nn = conv2d_no_bias(nn, stem_width * 2, 3, strides=1, padding="same", name=name+"3_")
+    else:
+        nn = conv2d_no_bias(inputs, stem_width, 7, strides=2, padding="same", name=name+"3_")
+
+    nn = batchnorm_with_activation(nn, activation=activation, name=name+"3_")
+    nn = layers.MaxPool2D(pool_size=3, strides=2, padding="same", name=name+"pool")(nn)
+    return nn
+
+
+def ResNest(input_shape, blocks_set=[3, 4, 6, 3], stem_width=32, classes=1000, activation="relu", radix=2, model_name="resnest", **kwargs):
+    img_input = layers.Input(shape=input_shape)
+    nn = stem(img_input, stem_width, deep_stem=True, name="stem_")
+
+    nn = stack(nn, blocks=blocks_set[0], filters=64, strides=1, radix=radix, name="stack1_")
+    nn = stack(nn, blocks=blocks_set[1], filters=128, strides=2, radix=radix, name="stack2_")
+    nn = stack(nn, blocks=blocks_set[2], filters=256, strides=2, radix=radix, name="stack3_")
+    nn = stack(nn, blocks=blocks_set[3], filters=512, strides=2, radix=radix, name="stack4_")
+
+    if classes > 0:
+        nn = layers.GlobalAveragePooling2D(name="avg_pool")(nn)
+        nn = layers.Dense(classes, activation="softmax", name="predictions")(nn)
+
+    model = tf.keras.models.Model(img_input, nn, name=model_name)
+    return model
+
+
+def ResNest50(input_shape=(112, 112, 3), stem_width=32, classes=1000, activation="relu", radix=2, model_name="ResNest50", **kwargs):
+    return ResNest(blocks_set=[3, 4, 6, 3], **locals())
+
+
+def ResNest101(input_shape=(112, 112, 3), stem_width=64, classes=1000, activation="relu", radix=2, model_name="ResNest101", **kwargs):
+    return ResNest(blocks_set=[3, 4, 23, 3], **locals())
