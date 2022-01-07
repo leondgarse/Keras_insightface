fb178639b7fb00f9fd881da91327931ee50b17f5
leondgarse <leondgarse@gmail.com>
2022-01-07 14:26:15 +0800

    combine MagFace with CurricularFace

--- losses.py
+++ losses.py
@@ -166,7 +166,7 @@ class CurricularFaceLoss(ArcfaceLossSimple):
 
         self.hard_scale.assign(tf.reduce_mean(y_pred_vals) * 0.01 + (1 - 0.01) * self.hard_scale)
         tf.print(", hard_scale:", self.hard_scale, end="")
-        hard_norm_logits = tf.where(norm_logits > tf.expand_dims(theta, 1), norm_logits * (self.hard_scale + norm_logits), norm_logits)
+        hard_norm_logits = tf.where(norm_logits > tf.expand_dims(theta_valid, 1), norm_logits * (self.hard_scale + norm_logits), norm_logits)
 
         arcface_logits = tf.tensor_scatter_nd_update(hard_norm_logits, pick_cond, theta_valid) * self.scale
         return tf.keras.losses.categorical_crossentropy(y_true, arcface_logits, from_logits=self.from_logits, label_smoothing=self.label_smoothing)
@@ -222,6 +222,7 @@ class MagFaceLoss(ArcfaceLossSimple):
         scale=64.0,
         regularizer_loss_lambda=35.0,
         use_cosface_margin=False,
+        curricular_hard_scale=-1,
         from_logits=True,
         label_smoothing=0,
         **kwargs
@@ -231,10 +232,15 @@ class MagFaceLoss(ArcfaceLossSimple):
         self.min_feature_norm, self.max_feature_norm, self.regularizer_loss_lambda = min_feature_norm, max_feature_norm, regularizer_loss_lambda
         # l_margin, u_margin
         self.min_margin, self.max_margin = min_margin, max_margin
-        self.use_cosface_margin = use_cosface_margin
+        self.use_cosface_margin, self.curricular_hard_scale = use_cosface_margin, curricular_hard_scale
         self.margin_scale = (max_margin - min_margin) / (max_feature_norm - min_feature_norm)
         self.regularizer_loss_scale = 1.0 / (self.max_feature_norm ** 2)
+        self.use_curricular_scale = False
+        if curricular_hard_scale >= 0:
+            self.curricular_hard_scale = tf.Variable(curricular_hard_scale, dtype="float32", trainable=False)
+            self.use_curricular_scale = True
         # np.set_printoptions(precision=4)
+        # self.precission_4 = lambda xx: tf.math.round(xx * 10000) / 10000
 
     def call(self, y_true, norm_logits_with_norm):
         # feature_norm is multiplied with -1 in NormDense layer, keeping low for not affecting accuracy metrics.
@@ -243,44 +249,49 @@ class MagFaceLoss(ArcfaceLossSimple):
         # margin = (self.u_margin-self.l_margin) / (self.u_a-self.l_a)*(x-self.l_a) + self.l_margin
         margin = self.margin_scale * (feature_norm - self.min_feature_norm) + self.min_margin
 
+        pick_cond = tf.where(y_true != 0)
+        y_pred_vals = tf.gather_nd(norm_logits, pick_cond)
         if self.use_cosface_margin:
             # Cosface process
-            arcface_logits = tf.where(tf.cast(y_true, dtype=tf.bool), norm_logits - margin, norm_logits) * self.scale
+            # arcface_logits = tf.where(tf.cast(y_true, dtype=tf.bool), norm_logits - margin, norm_logits) * self.scale
+            theta_valid = y_pred_vals - margin
         else:
             # Arcface process
             margin_cos, margin_sin = tf.cos(margin), tf.sin(margin)
             threshold = tf.cos(np.pi - margin)
-            pick_cond = tf.where(y_true != 0)
-            y_pred_vals = tf.gather_nd(norm_logits, pick_cond)
             theta = y_pred_vals * margin_cos - tf.sqrt(1 - tf.pow(y_pred_vals, 2)) * margin_sin
             theta_valid = tf.where(y_pred_vals > threshold, theta, self.theta_min - theta)
-            arcface_logits = tf.tensor_scatter_nd_update(norm_logits, pick_cond, theta_valid) * self.scale
+
+        if self.use_curricular_scale:
+            self.curricular_hard_scale.assign(tf.reduce_mean(y_pred_vals) * 0.01 + (1 - 0.01) * self.curricular_hard_scale)
+            tf.print(", hard_scale:", self.curricular_hard_scale, end="")
+            norm_logits = tf.where(norm_logits > tf.expand_dims(theta_valid, 1), norm_logits * (self.curricular_hard_scale + norm_logits), norm_logits)
+
+        arcface_logits = tf.tensor_scatter_nd_update(norm_logits, pick_cond, theta_valid) * self.scale
         arcface_loss = tf.keras.losses.categorical_crossentropy(y_true, arcface_logits, from_logits=self.from_logits, label_smoothing=self.label_smoothing)
 
         # MegFace loss_G, g = 1/(self.u_a**2) * x_norm + 1/(x_norm)
         regularizer_loss = self.regularizer_loss_scale * feature_norm + 1.0 / feature_norm
 
-        # tf.print(", arcface_loss:", tf.reduce_mean(arcface_loss), end="")
         tf.print(
-            " -",
-            # " regularizer_loss: ",
+            # ", regularizer_loss: ",
             # tf.reduce_mean(regularizer_loss),
-            " arcface_loss: ",
+            ", arcface: ",
             tf.reduce_mean(arcface_loss),
-            ", margin mean: ",
+            ", margin: ",
             tf.reduce_mean(margin),
-            ", min: ",
-            tf.reduce_min(margin),
-            ", max: ",
-            tf.reduce_max(margin),
-            ", feature_norm mean: ",
+            # ", min: ",
+            # tf.reduce_min(margin),
+            # ", max: ",
+            # tf.reduce_max(margin),
+            ", feature_norm: ",
             tf.reduce_mean(feature_norm),
             # ", min: ",
             # tf.reduce_min(feature_norm),
             # ", max: ",
             # tf.reduce_max(feature_norm),
             sep="",
-            end="",
+            end="\r",
         )
         return arcface_loss + regularizer_loss * self.regularizer_loss_lambda
 
@@ -294,6 +305,7 @@ class MagFaceLoss(ArcfaceLossSimple):
                 "max_margin": self.max_margin,
                 "regularizer_loss_lambda": self.regularizer_loss_lambda,
                 "use_cosface_margin": self.use_cosface_margin,
+                "curricular_hard_scale": K.get_value(self.curricular_hard_scale),
             }
         )
         return config
